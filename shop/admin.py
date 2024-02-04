bd2c69f630e7a0453547bf0e1f5356c743cf8f46
Rahul <rahul9380767545@gmail.com>
2024-02-04 15:22:18 +0530

    Customized Admin Panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Product)
+# admin panel header
+admin.site.site_header = "My Shop"
+
+# admin panel title
+admin.site.site_title = "My Shop"
+
+# admin panel index title
+admin.site.index_title = "Admin"
+
+class ProductAdmin(admin.ModelAdmin):
+    def change_category_to_sports(self, request, queryset):
+        queryset.update(category="Sports")
+
+    def change_category_to_riding_gears(self, request, queryset):
+        queryset.update(category="Riding Gears")
+
+    def change_category_to_laptops(self, request, queryset):
+        queryset.update(category="Laptops")
+
+    def change_category_to_mobiles(self, request, queryset):
+        queryset.update(category="Mobile Phones")
+
+    change_category_to_sports.short_description = 'Sports Category'
+    change_category_to_riding_gears.short_description = 'Riding Gears Category'
+    change_category_to_laptops.short_description = 'Laptops Category'
+    change_category_to_mobiles.short_description = 'Mobile Phones Category'
+
+
+    list_display = ('title', 'price', 'discount_price', 'category', 'description', 'image')
+    list_editable = ('price',)
+    search_fields = ('title', 'category', 'description')
+    actions = ('change_category_to_sports', 'change_category_to_riding_gears', 'change_category_to_laptops', 'change_category_to_mobiles')
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
